5b25cf42429386e5cb939d4e1b55f40ba65ebb98
shall <1261370234zzd@Gmail.com>
2026-08-25 14:25:04 +0800

    feat: 新增员工功能
    - 新增员工接口 POST /admin/employee
    - 员工密码MD5加密处理
    - 重复用户名异常处理
    - 使用ThreadLocal动态获取创建人/修改人ID
    - 添加Swagger接口文档注解

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -43,6 +43,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      */
     @Bean
     public Docket docket() {
+        log.info("开始生成接口文档...");
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("苍穹外卖项目接口文档")
                 .version("2.0")
@@ -54,6 +55,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
                 .paths(PathSelectors.any())
                 .build();
+        log.info("接口文档生成完成，扫描包：com.sky.controller");
         return docket;
     }
 
@@ -62,7 +64,11 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @param registry
      */
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始设置静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        log.info("静态资源映射[/doc.html] -> classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        log.info("静态资源映射[/webjars/**] -> classpath:/META-INF/resources/webjars/");
+        log.info("静态资源映射设置完成");
     }
 }
